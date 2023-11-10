c1f65f05910af10f70404f5f78d1f4bc760d12b4
Dhruv Gupta <dhruv035@gmail.com>
2023-11-10 16:37:53 +0530

    Cliff Processing + Next Vesting Calculation

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,7 @@ const Home: NextPage = () => {
   const [schedule,setSchedule]=useState<VestingSchedule|null>(null)
   const { address } = useAccount();
   const { data: vestingSchedule } = useContractRead({
+    watch:true,
     address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as `0x${string}`,
     abi: [...contract.abi],
     functionName: "getVestingSchedule",
@@ -36,7 +37,14 @@ const Home: NextPage = () => {
     args: [address],
 
   });
+  const currentDate = new Date();
+  const timestamp = Math.floor(currentDate.getTime())
 
+  if(schedule&&schedule.beneficiary!==nullAddress)
+  {
+    console.log("TIMESTAMP",timestamp,schedule.start,schedule.cliff,schedule.slicePeriodSeconds)
+    console.log("NEXT PERIOD")
+  }
 
   console.log("VESTING", vestingSchedule);
   return (
@@ -61,12 +69,12 @@ const Home: NextPage = () => {
               <div className="text-[2vw] text-sky-200">
                 <p className="font-bold">Total Allocation: {Number(schedule.amountTotal)}</p>
                 <p className="font-bold">Allocation Date: {(new Date(Number(schedule.start))).toLocaleDateString()}</p>
-                <p className="font-bold">Cliff: {(Number(schedule.cliff))}</p>
+                <p className="font-bold">Cliff: {(new Date(Number(schedule.cliff))).toLocaleDateString()}</p>
                 <p className="font-bold">Frequency: {(Number(schedule.slicePeriodSeconds))}</p>
                 <p className="font-bold">Duration: {(Number(schedule.duration))}</p>
                 <p className="font-bold">Claimed: {(Number(schedule.released))}</p>
                 <p className="font-bold">Available:{(Number(availableAmount))}</p>
-                <p className="font-bold">Next Vesting On</p>
+                <p className="font-bold">Next Vesting In: {Number(schedule.slicePeriodSeconds)-Math.floor((timestamp-Number(schedule.cliff))/1000)%Number(schedule.slicePeriodSeconds)}</p>
                 <button className="border-blue-800 border-[1px] bg-yellow-400 text-black p-2 rounded-[10px] mt-4">Claim Now</button>
               </div>
             ) : (
